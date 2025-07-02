2ff6210a8bd00cd9c45b805f33266b040f362de5
William Favorite <wfavorit@gmail.com>
2025-07-02 10:09:25 -0400

    Markdown additon. Push to see how it looks.

--- version.go
+++ version.go
@@ -16,19 +16,21 @@ package initq
 				     it 'correct'.
 	0.2.0   25-7-2 - Aligned struct, method, and other naming to match the
 	                 module name. (Removed RunQ references.)
+				   - Added markdown. (Several commits are sure to follow to
+				     get the content right.)
 */
 
 // VersionString is the version of the project.
-const VersionString = "0.1.0"
+const VersionString = "0.2.0"
 
 /*
 	ToDos:
     [_] Improve documentation.
 	[ ] Proper module name.
-	[ ] Clean up remaining previous module name references. To include file
-	    renames.
 
 	Done:
+	[X] Clean up remaining previous module name references. To include file
+	    renames.
 	[Q] Author. (Don't do this if you intend to publish.)
 	[X] Consider a rename. "runq" suggests other things. While avoiding startq,
 	    as a name... that leaves... "initq"? "upq"?
